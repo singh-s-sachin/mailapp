08a60fe4ceb0a5a76104bf134d6aada0f855122a
Sachin <singh.s.kshetriya@gmail.com>
2019-06-18 18:40:07 +0530

    Api work

--- mail/polls/views.py
+++ mail/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+import json
 # Create your views here.
 def email(request):
 	send_mail( 'Ionixx Technologies', 'You are hired', 'sachinkumar.s@ionixxtech.com', ['singh.s.kshetriya@gmail.com'] )
